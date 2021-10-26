78cf9d2ae779556bff68480816e107dfdcbd225d
CodyD604 <cody604604@gmail.com>
2021-10-26 19:30:31 -0400

    Add basic component with scss styling working

---
+++ app/pods/components/service-header/component.ts
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import ServiceModel from 'bpfcaudit-ui/models/service';
+
+export default class ServiceHeader extends Component {
+  service?: ServiceModel; // TODO: tracked?
+}
